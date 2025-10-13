132556bd76dc9b06c536f281b94ce5122ae0a837
TanguyBonfitt0 <tanguy.bonfitto@supinfo.com>
2025-10-13 20:39:57 +0200

    readme + page localhost

--- food-express/routes/index.js
+++ food-express/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '3APIS - FoodExpress',
+        message: 'On vous invite à tester le projet sur Postman !'
+    });
 });
+
 
 module.exports = router;
